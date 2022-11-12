bd5ba468a3b15ce610b45b762505a4b4d33db96b
luiscursos <47142045+luiscursos@users.noreply.github.com>
2022-11-13 00:10:29 +0100

    Update Time.py

--- Time.py
+++ Time.py
@@ -9,7 +9,7 @@ haréis una operación para calcular el tiempo que queda de trabajo.
 """
 current = localtime()[3] # 
 
-if current >= strptime(("07:00"[:2]),"%H")[3]:
+if current >= strptime(("07:00"[:2]),"%M%H")[3]:
      print("It's time go to home")
 else:
      work_time_left = strptime("07:00"[:2],"%M%H")[3]-current
