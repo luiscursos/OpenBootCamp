dc83afad9eedc470fbcdcc37971808aecf13727b
luiscursos <47142045+luiscursos@users.noreply.github.com>
2022-11-19 15:21:08 +0100

    Update Ejercicio_9.2.py

--- Ejercicio_9.2.py
+++ Ejercicio_9.2.py
@@ -1,3 +1,4 @@
+from functools import reduce
 # En este segundo ejercicio, tenéis que crear una aplicación que obtendrá los elementos impares de una lista pasada por parámetro con filter y realizará una suma de todos estos elementos obtenidos mediante reduce.
 
 
@@ -6,6 +7,10 @@ def odd_numbers (number):
     if number % 2 != 0:
         return True
 
+def add (a,b):
+    return a+b
+
 numbers_list = [1,23,45,4,67,8,6,10,14,11,14,82]
-print(list(filter(odd_numbers,numbers_list)))
+odd_result=(list(filter(odd_numbers,numbers_list)))
+print(reduce(add,odd_result))
 
